7d957e8ba3d5df95f5e2f0876aad2c37c2521671
Stephen Ball <stephen@rebelinblue.com>
2016-12-03 21:39:49 +0000

    Finishing GitHub tests

--- tests/Http/Webhooks/GithubTest.php
+++ tests/Http/Webhooks/GithubTest.php
@@ -41,7 +41,32 @@ class GithubTest extends TestCase
         return $request;
     }
 
-    private function mockRequestWithPayload($payload)
+    private function mockPullRequest()
+    {
+        $request = $this->mockEventRequest('push');
+
+        $payload = $this->getMockBuilder(ParameterBag::class)
+                        ->disableOriginalConstructor()
+                        ->getMock();
+
+        $request->expects($this->once())
+                ->method('json')
+                ->willReturn($payload);
+
+        $payload->expects($this->once())
+                ->method('has')
+                ->with($this->equalTo('after'))
+                ->willReturn(true);
+
+        $payload->expects($this->once())
+                ->method('get')
+                ->with($this->equalTo('after'))
+                ->willReturn('0000000000000000000000000000000000000000');
+
+        return $request;
+    }
+
+    private function mockRequestWithPayload(array $data, $ref)
     {
         $request = $this->mockEventRequest('push');
 
@@ -58,35 +83,73 @@ class GithubTest extends TestCase
                 ->with($this->equalTo('after'))
                 ->willReturn(false);
 
+        $payload->expects($this->at(1))
+                ->method('get')
+                ->with($this->equalTo('head_commit'))
+                ->willReturn($data);
+
+        $payload->expects($this->at(2))
+                ->method('get')
+                ->with($this->equalTo('ref'))
+                ->willReturn('refs/' . $ref);
+
         return $request;
     }
 
     public function testIsRequestOriginValid()
     {
         $request = $this->mockRequestIsFromGithub(true);
-        $github = new Github($request);
 
+        $github = new Github($request);
         $this->assertTrue($github->isRequestOrigin());
     }
 
     public function testIsRequestOriginInvalid()
     {
         $request = $this->mockRequestIsFromGithub(false);
-        $github = new Github($request);
 
+        $github = new Github($request);
         $this->assertFalse($github->isRequestOrigin());
     }
 
-    // FIXME: Still need to handle the fake push with 00000 etc
-
-    public function testHandlePushEventValid()
+    public function testHandleClosedPullRequest()
     {
-        $request = $this->mockRequestWithPayload([]);
+        $request = $this->mockPullRequest();
 
         $github = new Github($request);
+        $this->assertFalse($github->handlePush());
+    }
 
-        $github->handlePush();
+    /**
+     * @param string $branch
+     * @param string $ref
+     * @dataProvider getBranch
+     */
+    public function testHandlePushEventValid($branch, $ref)
+    {
+        $expected = [
+            'message'   => 'Commit log',
+            'url'       => 'http://www.example.com/',
+            'id'        => 'abc123456xyz',
+            'committer' => [
+                'name'  => 'John Smith',
+                'email' => 'john.smith@example.com'
+            ]
+        ];
 
+        $request = $this->mockRequestWithPayload($expected, $ref);
+
+        $github = new Github($request);
+        $actual = $github->handlePush();
+
+        $this->assertWebhookDataIsValid($expected, $actual, $branch, 'Github');
+    }
+
+    public function getBranch() {
+        return [
+            'Branch' => ['master', 'heads/master'],
+            'Tag'    => ['1.0.0', 'tags/1.0.0']
+        ];
     }
 
     /**
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,4 +22,25 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         return $app;
     }
+
+    protected function assertWebhookDataIsValid($expected, $actual, $branch, $source)
+    {
+        $this->assertInternalType('array', $actual);
+
+        $this->assertArrayHasKey('reason', $actual);
+        $this->assertArrayHasKey('branch', $actual);
+        $this->assertArrayHasKey('source', $actual);
+        $this->assertArrayHasKey('build_url', $actual);
+        $this->assertArrayHasKey('commit', $actual);
+        $this->assertArrayHasKey('committer', $actual);
+        $this->assertArrayHasKey('committer_email', $actual);
+
+        $this->assertEquals($expected['message'], $actual['reason']);
+        $this->assertEquals($branch, $actual['branch']);
+        $this->assertEquals($source, $actual['source']);
+        $this->assertEquals($expected['url'], $actual['build_url']);
+        $this->assertEquals($expected['id'], $actual['commit']);
+        $this->assertEquals($expected['committer']['name'], $actual['committer']);
+        $this->assertEquals($expected['committer']['email'], $actual['committer_email']);
+    }
 }
